c9436496f71c97f62a0cad77b6b13164ea3726fc
samansau <samantsaurabh@gmail.com>
2018-05-05 21:11:13 +0530

    new changes

--- app/src/main/java/android/dev/personalassistant/kym/KnowYourMasterActivity.java
+++ app/src/main/java/android/dev/personalassistant/kym/KnowYourMasterActivity.java
@@ -27,7 +27,7 @@ public class KnowYourMasterActivity extends AppCompatActivity {
 
     private void populateKymTabs(){
         ViewPager viewPager = (ViewPager) findViewById(R.id.kymViewPager);
-        String kymTitles[]=new String[]{"Per", "Fam", "Fin","Nos"};
+        String kymTitles[]=new String[]{"Personal", "Family", "Financial"};
         TabFragment kymTabFragment=new KymTabFragment();
         TabAdapter adapter = new TabAdapter(getSupportFragmentManager(),kymTitles,kymTabFragment);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.kymTabs);
@@ -35,7 +35,10 @@ public class KnowYourMasterActivity extends AppCompatActivity {
 
     }
 
-
+    public void showResidentialDetails(View view){
+        Intent intent=new Intent(this,KymShowResidentialDetailsActivity.class);
+        startActivity(intent);
+    }
 
 
 
@@ -47,6 +50,11 @@ public class KnowYourMasterActivity extends AppCompatActivity {
 
     public void showBankDetails(View view){
         Intent intent=new Intent(this,KymShowBankDetailsActivity.class);
+        startActivity(intent);
+    }
+
+    public void showCarDetails(View view){
+        Intent intent=new Intent(this,KymShowCarDetailsActivity.class);
         startActivity(intent);
     }
 
